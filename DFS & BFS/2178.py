813aa6c2736af9e493c2a9f87c1353112dc2d8b7
goongju <titu1015@gmail.com>
2021-12-09 16:55:10 +0900

    study about DFS & BFS

---
+++ DFS & BFS/2178.py
@@ -0,0 +1,20 @@
+n,m=map(int, input().split())
+graph=[list(map(int,input())) for _ in range(n)]
+
+dx=[1,-1,0,0]
+dy=[0,0,-1,1]
+queue=[[0,0]]
+
+while queue:
+    x,y=queue[0][0],queue[0][1]
+    del queue[0]
+    for i in range(4):
+
+        nx=x+dx[i]
+        ny=y+dy[i]
+
+        if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1:
+                queue.append([nx,ny])
+                graph[nx][ny]=graph[x][y]+1
+print(graph[n-1][m-1])
+
--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-a=input()
-print(a)
+
+
+def solution(number, k):
+    lst=sorted(list(number))
+    answer=list(number)
+    n=0
+    for i in lst:
+        while i in answer:
+            if n>=k:
+                return "".join(answer)
+            answer.remove(i)
+            n+=1
+print(solution("4177252841",4))
